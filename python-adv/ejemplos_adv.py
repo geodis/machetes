1157ff9919c7e9b5d908fdfa7f8dfa1723b256b2
geodis <geodis@gmail.com>
2020-03-03 00:24:44 -0300

    py

--- python-adv/ejemplos_adv.py
+++ python-adv/ejemplos_adv.py
@@ -1,17 +1,17 @@
 # class Dog:
-# 	def __init__(self):
-# 		self.bark()
+#   def __init__(self):
+#       self.bark()
 
 # =================================
 # def make_class(x):
-# 	class Dog:
-# 		def __init__(self, name):
-# 			self.name = name
+#   class Dog:
+#       def __init__(self, name):
+#           self.name = name
 
-# 		def print_value(self):
-# 			print(x)
+#       def print_value(self):
+#           print(x)
 
-# 	return Dog
+#   return Dog
 
 # cls = make_class(10)
 # print("-----------")
@@ -27,24 +27,24 @@
 
 
 # for i in range(10):
-# 	def show():
-# 		print(i*2)
-# 		print("**")
+#   def show():
+#       print(i*2)
+#       print("**")
 
-# 	print("--")
-# 	show()	
+#   print("--")
+#   show()  
 
 # =================================
 
 # import inspect
 # def func(x):
-# 	if x == 1:
-# 		def rv():
-# 			print("x es 1")
-# 	else:
-# 		def rv():
-# 			print("x != 1")
-# 	return rv
+#   if x == 1:
+#       def rv():
+#           print("x es 1")
+#   else:
+#       def rv():
+#           print("x != 1")
+#   return rv
 
 # new_func = func(12)
 # new_func()
@@ -67,8 +67,84 @@ dumpear librerias
 
 # =================================
 
-import inspect
+# import inspect
 
-print(inspect.getsource(list))
-x = [1,2,3]
-y = [4,5]
+# # list es un built-in class por lo que no puedo ver el source
+# # print(inspect.getsource(list))
+
+# x = [1,2,3]
+# y = [4,5]
+
+# =================================
+
+# class Person:
+#     # DataModel methods
+#     # https://docs.python.org/3/reference/datamodel.html
+#     def __init__(self,name):
+#         self.name = name
+
+#     def __repr__(self):
+#         return f"Person({self.name})"
+
+#     def __mul__(self,x):
+#         if type(x) is not int:
+#             raise Exception("Invalid argument, must be int")
+
+#         self.name = self.name * x
+
+#     def __call__(self,y):
+#         print("called this function", y)
+
+#     def __len__(self):
+#         return len(self.name)
+
+
+# # ej: __repr__ 
+# p = Person("pepe")
+# print(p)
+
+# # ej: __mul__ 
+# p * 4
+# print(p)
+
+# # ej: __call__
+# p(10) 
+
+# # ej: __len__
+# print(len(p))
+
+# =================================
+
+# from queue import Queue as q
+# import inspect
+
+# # Queue es mi clase que hereda de q
+# class Queue(q):
+#     # la Queue original no define __repr__
+#     def __repr__(self):
+#         # la Queue original implementa _qsize()
+#         return f"Queue({self._qsize()})"
+
+#     def __add__(self, item):
+#         self.put(item)
+
+#     def __sub__(self, item):
+#         self.get()
+
+
+# qu = Queue()
+# # llamo a __add__
+# print(qu)
+# qu + 9
+# print(qu)
+# qu + 7
+# print(qu)
+# # llamo a __sub__
+# qu - 1
+# print(qu)
+
+# =================================
+
+# Metaclases
+# ----------
+
